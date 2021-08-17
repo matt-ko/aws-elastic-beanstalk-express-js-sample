b7c9b401441bac00c117a95c3afe43f46292c33d
Mathias Koech <koech.matt@gmail.com>
2021-08-17 21:41:50 +0300

    Cleaning up folows

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello DevOps!! I just added a manual review'));
+app.get('/', (req, res) => res.send('Hello DevOps!! Now I have to delete everything!!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
